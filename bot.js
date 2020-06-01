38fa7802c495716ab45afe5e3121bcfe6fafcc80
Julian Duque <juliandduque@hotmail.com>
2020-06-01 02:26:33 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,7 +26,7 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.CLIENT_TOKEN);//BOT_TOKEN is the Client Secret
+client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
 // const Discord = require('discord.js');
 
 // const bot = new Discord.Client();
