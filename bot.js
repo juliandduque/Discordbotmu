4d7a97577a9097f5f8dff0eb9970f72104571d62
Julian Duque <juliandduque@hotmail.com>
2020-06-01 01:11:30 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,55 +1,65 @@
 var Discord = require('discord.io');
-var logger = require('winston');
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-    colorize: true
-});
 
-logger.level = 'debug';
 // Initialize Discord Bot
-var bot = new Discord.Client({
-    token: process.env.token,
-    autorun: true
+var bot = new Discord.Client();
+
+bot.on("ready", function () {
+	console.log("Ready to begin! Serving in " + bot.channels.length + " channels");
 });
 
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
+client.on("message", message => 
+{
+    if (message.author.bot) return;
 
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ')
-        var cmd = args[0];
+    // The process.env.PREFIX is your bot's prefix in this case.
+    if (message.content.indexOf('!') !== 0) return;
+    
+    var args = message.content.substring(1).split(' ')
+    var command = args[0];
+    
+    switch (command) 
+    {
+        case 'ping':
+            bot.reply(message, "pong");
+            break;
+            
+        case 'god of darkness':
+            
+            var server = 1
+            
+            if (args.length > 2)
+            {
+                server = args[1]
+            }
+            
+            bot.reply(message, 'Muerte de God of Darkness reportada en server: ' + server);
 
-        args = args.splice(1);
-        switch (cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-                break;
-            case 'boss':
-                bot.sendMessage({
-                    to: '529288037717377035',
-                    message: 'Muerte de God of Darkness reportada por ' + user
-                });
-                setTimeout(function () {
-                    bot.sendMessage({
-                        to: '529288037717377035',
-                        message: 'Ventana del boss esta abierta por 2 horas @everyone'
-                    });
-                }, 14400000);
-                
-                break;
+            setTimeout(function () 
+            {
+                bot.reply(message, 'Ventana del god of darkness esta abierta por 2 horas en el server ' + server + ' @here');
+            }, 14400000);
+            
+            break;
+            
+        case 'elite':
+            
+            var server = 1
+            
+            if (args.length > 2)
+            {
+                server = args[1]
+            }
+            
+            bot.reply(message, 'Muerte de God of Darkness reportada en server: ' + server);
 
-            // Just add any case commands if you want to..
-        }
+            setTimeout(function () 
+            {
+                bot.reply(message, 'Mounstros elite salen en 5 minutos en el server ' + server + ' @here');
+            }, 43500000);
+            
+            break;  
+            
     }
 });
+
+client.login();
