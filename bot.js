d18cbe30bfa6fe8b5a921d105ec442146168171c
Julian Duque <juliandduque@hotmail.com>
2020-06-02 17:18:59 -0400

    test

--- bot.js
+++ bot.js
@@ -241,7 +241,17 @@ bot.on("message", message => {
                 { 
                     'security-key': 'JorgeEsGay',
                     'Cache-Control': 'no-cache, no-store'
-                } 
+                }
+                "body":
+                {
+                    sender2: 
+                    {
+                        "personaje": personaje2,
+                        "raza": raza2,
+                        "guild": guild2,
+                        "radiance": radiance2
+                    }  
+				}
             };
             let http2 = require('http');
             let req = http2.request(options, function (res) {
@@ -258,18 +268,6 @@ bot.on("message", message => {
                         message.reply("Inscrito al evento: " + lista2);
                 });
             });
-            
-            req.write(qs.stringify({
-                  sender2: 
-                  {
-                    "personaje": personaje2,
-                    "raza": raza2,
-                    "guild": guild2,
-                    "radiance": radiance2
-                  }
-            }));
-
-            req.end();
     }
 });
             
