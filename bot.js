11dd4ff073cb8642790dbded887cc8c14fb96cb4
Julian Duque <juliandduque@hotmail.com>
2020-06-23 13:28:07 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -49,6 +49,7 @@ var picabot = [
     "Picahielo entered the game",
     "Porque hablan tanto sobre mi?",
     "hay dias que si",
+    "mas bien, preguntate porque nuestr@ master es un@ travesti peli roja",
     "has escuchado la historia del hombre con 3 penes? Una vez estaba midiendose el pantalon, y cuando le midieron la entrepierna, le preguntaron, a que lado te pones el pene? El respondio: si",
     "shi shenol",
     "acabo de hacer una mision gens"
