0a551b24cf8b35b9f16c530ee935359bba8cc046
Julian Duque <juliandduque@hotmail.com>
2020-07-23 21:25:36 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -87,7 +87,7 @@ bot.on("message", message => {
     //if (message.content.toLowerCase().includes('picabot')){ message.reply(picabot[Math.floor(Math.random() * picabot.length)]);
     //}
     if (message.content.indexOf('?') !== 0) return;
-    message.reply('ozuna')
+    message.reply('disfruten de ozuna :) ')
     return;
     let args = message.content.substring(1).split(' ')
     let command = args[0].toLowerCase();
